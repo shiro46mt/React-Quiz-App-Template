993aad6ef8fb49287f935374be66477050b072c3
shiro46mt <39456646+shiro46mt@users.noreply.github.com>
2024-07-15 23:41:40 +0900

    結果表示のカスタマイズ

--- src/components/ResultScreen/ResultOverview/index.tsx
+++ src/components/ResultScreen/ResultOverview/index.tsx
@@ -27,23 +27,17 @@ interface ResultOverviewProps {
 const ResultOverview: FC<ResultOverviewProps> = ({ result }) => {
   const { quizDetails, endTime } = useQuiz()
 
-  const totalQuestionAttempted = result.length
-
   const obtainedScore = result
     .filter((item) => item.isMatch && typeof item.score === 'number')
     .reduce((accumulator, currentValue) => accumulator + (currentValue.score || 0), 0)
 
   // Passed if 60 or more than 60% marks
   const calculateStatus =
-    (obtainedScore / quizDetails.totalScore) * 100 >= 60 ? 'Passed' : 'Failed'
+    endTime <= quizDetails.totalTime ? 'Passed' : 'Failed'
+    // (obtainedScore / quizDetails.totalScore) * 100 >= 60 ? 'Passed' : 'Failed'
 
   return (
     <ResultOverviewStyle>
-      <p>
-        You attempted questions:{' '}
-        <HighlightedText> {totalQuestionAttempted} </HighlightedText>/{' '}
-        {quizDetails.totalQuestions}
-      </p>
       <p>
         Score secured:<HighlightedText> {obtainedScore} </HighlightedText>/{' '}
         {quizDetails.totalScore}
